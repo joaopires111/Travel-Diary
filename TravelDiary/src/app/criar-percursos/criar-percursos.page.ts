2718c2d40b48b46f97ab1d01849e3dca92056f0f
jfss1 <jose.filipe.sampaio@live.com.pt>
2021-06-30 21:11:31 +0100

    Pontos no criarPercursos

--- TravelDiary/src/app/criar-percursos/criar-percursos.page.ts
+++ TravelDiary/src/app/criar-percursos/criar-percursos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { Ponto, StorageService } from '../services/storage.service';
+import { Percurso, Ponto, StorageService } from '../services/storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-criar-percursos',
@@ -8,7 +9,64 @@ import { Ponto, StorageService } from '../services/storage.service';
   styleUrls: ['./criar-percursos.page.scss'],
 })
 export class CriarPercursosPage {
-  constructor(private router: Router) { }
+  pontos: Ponto[] = [];
+  newPonto: Ponto = <Ponto>{};
+
+  newPontos: Ponto[] = [];
+  newPercurso: Percurso = <Percurso>{};
+
+  @ViewChild('myList')myList: IonList;
+  
+  constructor(private router: Router, private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(()=>{
+      this.loadPontos();
+    })
+   }
+
+   /*addPonto(){
+     this.newPonto.id = Date.now();
+
+     this.storageService.addPonto(this.newPonto).then(ponto => {
+       this.newPonto = <Ponto>{};
+       this.showToast('Ponto de Interesse criado');
+       this.loadPontos();
+     })
+   }*/
+
+   loadPontos(){
+     this.storageService.getPontos().then(pontos => {
+       this.pontos = pontos;
+       console.log(pontos[0].lugar);
+     })
+   };
+
+   /*updatePonto(ponto: Ponto){
+    ponto.lugar = `UPDATED: ${ponto.lugar}`;
+    ponto.nome = `UPDATED: ${ponto.nome}`;
+    ponto.tipo = `UPDATED: ${ponto.tipo}`;
+    
+    this.storageService.updatePonto(ponto).then(ponto => {
+      this.showToast('Ponto Atualizado');
+      this.myList.closeSlidingItems();
+      this.loadPontos();
+    });
+   }
+
+   deletePonto(ponto: Ponto){
+     this.storageService.deletePonto(ponto.id).then(ponto =>{
+       this.showToast('Ponto Removido');
+       this.myList.closeSlidingItems();
+       this.loadPontos();
+     });
+   }*/
+
+   async showToast(msg){
+     const toast = await this.toastController.create({
+       message: msg,
+       duration: 2000
+     });
+     toast.present();
+   }
 
   
   criar(){
