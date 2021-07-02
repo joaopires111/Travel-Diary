c37e686ed74800887cb31ad409032959ae460ff4
jfss1 <jose.filipe.sampaio@live.com.pt>
2021-07-02 18:11:33 +0100

    Sair da app

--- TravelDiary/src/app/app.component.ts
+++ TravelDiary/src/app/app.component.ts
@@ -1,15 +1,51 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+
+import { Platform, AlertController } from '@ionic/angular'
+
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(private router: Router) {}
-  public home(){
-    this.router.navigate(["home"]);
+  constructor(
+    private platform: Platform,
+
+    private _location: Location,
+    public alertController: AlertController
+  ) {
+    this.initializeApp();
   }
-  
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+    });
+
+  }
+
+  showExitConfirm() {
+    this.alertController.create({
+      header: 'App termination',
+      message: 'Do you want to close the app?',
+      backdropDismiss: false,
+      buttons: [{
+        text: 'Stay',
+        role: 'cancel',
+        handler: () => {
+          console.log('Application exit prevented!');
+        }
+      }, {
+        text: 'Exit',
+        handler: () => {
+          navigator['app'].exitApp();
+        }
+      }]
+    })
+      .then(alert => {
+        alert.present();
+      });
+  }
+
 }
